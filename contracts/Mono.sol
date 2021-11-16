5abddb4b76299746bdd14ea0fb97c3603252a0e0
unknown <franciscocurdi@gmail.com>
2021-11-16 00:00:25 -0300

    fix transferFrom

--- contracts/Mono.sol
+++ contracts/Mono.sol
@@ -8,16 +8,16 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 contract Mono is IERC20, IERC20Metadata {
     address public creator;
 
-    uint256 public override totalSupply = 1_000_000;
-
     string public override name = "Mono";
 
     string public override symbol = "MON";
 
     uint8 public override decimals = 18;
 
+    uint256 public override totalSupply = 1_000_000;
+
     mapping(address => uint256) public override balanceOf;
-    mapping(address => mapping(address => uint256)) allowances;
+    mapping(address => mapping(address => uint256)) _allowances;
 
     constructor() {
         creator = msg.sender;
@@ -42,7 +42,7 @@ contract Mono is IERC20, IERC20Metadata {
         override
         returns (uint256)
     {
-        return allowances[owner][spender];
+        return _allowances[owner][spender];
     }
 
     function approve(address spender, uint256 amount)
@@ -50,15 +50,8 @@ contract Mono is IERC20, IERC20Metadata {
         override
         returns (bool)
     {
-        address owner = msg.sender;
-        require(
-            balanceOf[owner] >= amount,
-            "Not enough funds to approve allowance"
-        );
-
-        uint256 newAllowance = allowances[owner][spender] + amount;
-        allowances[owner][spender] = newAllowance;
-        emit Approval(owner, spender, amount);
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
@@ -69,11 +62,10 @@ contract Mono is IERC20, IERC20Metadata {
     ) external override returns (bool) {
         require(balanceOf[sender] >= amount, "sender has not enough funds");
         require(
-            allowances[sender][recipient] >= amount,
+            _allowances[sender][msg.sender] >= amount,
             "not enough allowance"
         );
-
-        allowances[sender][recipient] -= amount;
+        _allowances[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(sender, recipient, amount);
--- test/Mono.js
+++ test/Mono.js
@@ -2,16 +2,17 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Mono token", () => {
-  let contract, ownerAccount, otherAccount;
+  let contract, ownerAccount, secondAccount, thirdAccount, secondSigner;
   const totalSupply = 1_000_000;
 
   beforeEach(async () => {
     const Mono = await ethers.getContractFactory("Mono");
     contract = await Mono.deploy();
     await contract.deployed();
-    [firstSigner, secondSigner, _] = await ethers.getSigners();
+    [firstSigner, secondSigner, thirdSigner, _] = await ethers.getSigners();
     ownerAccount = firstSigner.address;
-    otherAccount = secondSigner.address;
+    secondAccount = secondSigner.address;
+    thirdAccount = thirdSigner.address;
   });
 
   describe("Deployment", () => {
@@ -29,54 +30,45 @@ describe("Mono token", () => {
   describe("Transfer", () => {
     it("Should revert with Insufficient funds when amount is greater than sender's balance", async () => {
       await expect(
-        contract.transfer(otherAccount, totalSupply + 1)
+        contract.transfer(secondAccount, totalSupply + 1)
       ).to.be.revertedWith("Insufficient funds");
       expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
+      expect(await contract.balanceOf(secondAccount)).to.equal(0);
     });
     it("Should transfer correctly between two accounts", async () => {
       const amount = 1;
-      const receipt = await contract.transfer(otherAccount, amount);
+      const receipt = await contract.transfer(secondAccount, amount);
       expect(await contract.balanceOf(ownerAccount)).to.equal(
         totalSupply - amount
       );
-      expect(await contract.balanceOf(otherAccount)).to.equal(amount);
+      expect(await contract.balanceOf(secondAccount)).to.equal(amount);
       await expect(receipt)
         .to.emit(contract, "Transfer")
-        .withArgs(ownerAccount, otherAccount, amount);
+        .withArgs(ownerAccount, secondAccount, amount);
     });
   });
   describe("Approve", () => {
-    it("Should not approve allowance when amount is greater than balance", async () => {
-      await expect(
-        contract.approve(otherAccount, totalSupply + 1)
-      ).to.be.revertedWith("Not enough funds to approve allowance");
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
-      expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
-      expect(await contract.allowance(ownerAccount, otherAccount)).to.equal(0);
-    });
-
     it("Should approve allowance correctly", async () => {
       const amount = 100;
-      const receipt = await contract.approve(otherAccount, amount);
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
+      const receipt = await contract.approve(secondAccount, amount);
+      expect(await contract.balanceOf(secondAccount)).to.equal(0);
       expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
-      expect(await contract.allowance(ownerAccount, otherAccount)).to.equal(
+      expect(await contract.allowance(ownerAccount, secondAccount)).to.equal(
         amount
       );
       await expect(receipt)
         .to.emit(contract, "Approval")
-        .withArgs(ownerAccount, otherAccount, amount);
+        .withArgs(ownerAccount, secondAccount, amount);
     });
     it("Should approve allowance correctly multiple times", async () => {
       const amount1 = 100;
       const amount2 = 250;
-      await contract.approve(otherAccount, amount1);
-      await contract.approve(otherAccount, amount2);
-      expect(await contract.allowance(ownerAccount, otherAccount)).to.equal(
-        amount1 + amount2
+      await contract.approve(secondAccount, amount1);
+      await contract.approve(secondAccount, amount2);
+      expect(await contract.allowance(ownerAccount, secondAccount)).to.equal(
+        amount2
       );
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
+      expect(await contract.balanceOf(secondAccount)).to.equal(0);
       expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
     });
   });
@@ -84,53 +76,49 @@ describe("Mono token", () => {
   describe("TransferFrom", () => {
     it("Should not transfer when amount is greater than balance", async () => {
       await expect(
-        contract.transferFrom(ownerAccount, otherAccount, totalSupply + 1)
+        contract.transferFrom(ownerAccount, secondAccount, totalSupply + 1)
       ).to.be.revertedWith("sender has not enough funds");
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
+      expect(await contract.balanceOf(secondAccount)).to.equal(0);
       expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
     });
     it("Should not transfer when there is no allowance", async () => {
       await expect(
-        contract.transferFrom(ownerAccount, otherAccount, 1)
+        contract.transferFrom(ownerAccount, secondAccount, 1)
       ).to.be.revertedWith("not enough allowance");
-      expect(await contract.balanceOf(otherAccount)).to.equal(0);
+      expect(await contract.balanceOf(secondAccount)).to.equal(0);
       expect(await contract.balanceOf(ownerAccount)).to.equal(totalSupply);
     });
     it("Should transfer correctly some allowance", async () => {
       const amount = 1;
-      await contract.approve(otherAccount, amount * 2);
-      const receipt = await contract.transferFrom(
-        ownerAccount,
-        otherAccount,
+      await contract.approve(secondAccount, amount * 2);
+      const receipt = await contract
+        .connect(secondSigner) // secondAccount
+        .transferFrom(ownerAccount, thirdAccount, amount);
+      expect(await contract.allowance(ownerAccount, secondAccount)).to.equal(
         amount
       );
-      expect(await contract.allowance(ownerAccount, otherAccount)).to.equal(
-        amount
-      );
-      expect(await contract.balanceOf(otherAccount)).to.equal(amount);
+      expect(await contract.balanceOf(thirdAccount)).to.equal(amount);
       expect(await contract.balanceOf(ownerAccount)).to.equal(
         totalSupply - amount
       );
       await expect(receipt)
         .to.emit(contract, "Transfer")
-        .withArgs(ownerAccount, otherAccount, amount);
+        .withArgs(ownerAccount, thirdAccount, amount);
     });
     it("Should transfer correctly all allowance", async () => {
       const amount = 1;
-      await contract.approve(otherAccount, amount);
-      const receipt = await contract.transferFrom(
-        ownerAccount,
-        otherAccount,
-        amount
-      );
-      expect(await contract.allowance(ownerAccount, otherAccount)).to.equal(0);
-      expect(await contract.balanceOf(otherAccount)).to.equal(amount);
+      await contract.approve(secondAccount, amount);
+      const receipt = await contract
+        .connect(secondSigner) // secondAccount
+        .transferFrom(ownerAccount, thirdAccount, amount);
+      expect(await contract.allowance(ownerAccount, secondAccount)).to.equal(0);
+      expect(await contract.balanceOf(thirdAccount)).to.equal(amount);
       expect(await contract.balanceOf(ownerAccount)).to.equal(
         totalSupply - amount
       );
       await expect(receipt)
         .to.emit(contract, "Transfer")
-        .withArgs(ownerAccount, otherAccount, amount);
+        .withArgs(ownerAccount, thirdAccount, amount);
     });
   });
 });
