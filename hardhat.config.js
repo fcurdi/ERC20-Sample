f0fa558cc19af929fc5c91093f8157ab5ed02313
unknown <franciscocurdi@gmail.com>
2021-11-20 14:45:01 -0300

    ropsten network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +9,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const ROPSTEN_URL = process.env.INFURA_ROPSTEN_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: ROPSTEN_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
